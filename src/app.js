c2a11262c79857c30dd9287bd55bf93ba1cc2e09
Gaurav <gauravwagh0506@gmail.com>
2024-03-07 15:25:58 +0530

    added error class, response and async handler

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+// for managing who can make req to server;
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+// for limiting the amount of json reaches to the server;
+app.use(express.json({ limit: "16kb" }));
+
+// sometimes url include spaces, it will encode those url's;
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// it creates a directory named public in which all those files will be stored which we want to be stored on our server; 
+app.use(express.static("public"));
+
+// it allows us to set and get cookies stored in browser;
+app.use(cookieParser());
+
+export { app };
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err) => next(err));
+    }
+}
+
+export { asyncHandler };
+
+// this is a async handler which will process all async functions;
+
+// const asyncHandler = () => {};
+
+// const asyncHandler = (fn) =>{
+//     async()=>{
+//         fn();
+//     }
+// }
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         });
+//     }
+// }
